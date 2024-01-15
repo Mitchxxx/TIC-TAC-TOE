2f5d8c8e5d2cef18fea855c2b703ef55cbca4d86
Mitchel Egboko <megboko@gmail.com>
2024-01-15 11:20:21 +0000

    Update TIC-TAC-TOE project

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
-terraform {
+terraform{
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "mitchdev-cicd-terraform-eks"
+    key = "Github-Actions-EKS/terraform.tfstate"
+    region  = "eu-west-1"
   }
 }
